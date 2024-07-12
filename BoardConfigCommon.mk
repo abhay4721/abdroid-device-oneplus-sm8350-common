ef62a40840a4b1ee8b6c46727d6fcacf474b4d1a
abhay4721 <abhayabhi4721@gmail.com>
2024-07-12 19:22:01 +0530

    sm8350-common: Import QCC system_ext blobs
    Makes the QTI GNSS service happy. Thanks luca020400 for the help.
    
    Change-Id: I6de25b0b3fde6cce3a6cdf95f31c68734d429956

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -86,6 +86,7 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     vendor/everest/config/device_framework_matrix.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
+DEVICE_FRAMEWORK_MANIFEST_FILE += $(COMMON_PATH)/framework_manifest.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 ODM_MANIFEST_FILES := $(COMMON_PATH)/manifest_odm.xml
 
